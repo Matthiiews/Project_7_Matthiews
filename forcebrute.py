ad5ab491aaf3c77da57eaac64760d75692674698
Matthiiews <matthieu.kouassi@gmail.com>
2023-09-13 12:05:34 +0200

    Ajout et modification des fichiers

---
+++ forcebrute.py
@@ -0,0 +1,34 @@
+"""Effectue plusieurs opérations liées à la génération et à l'analyse de
+combinaisons d'actions financières à partir du fichier CSV
+"""
+
+# Import module
+import sys
+import time
+
+import combinations as cb
+
+MAX_BUDGET = float(sys.argv[2])
+
+filename = sys.argv[1]
+
+start = time.time()
+
+actions = cb.get_actions_from_csv(filename)
+nb_actions = len(actions)
+
+cb.show_combinations_number(nb_actions)
+
+combinations = []
+cb.get_all_combinations(actions, combinations)
+
+combinations_in_budget = []
+cb.get_all_combinations_in_budget(combinations, combinations_in_budget,
+                                  MAX_BUDGET)
+
+cb.calculate_combinations_profit(combinations_in_budget)
+
+combinations_in_budget.sort(key=lambda x: x[-1], reverse=True)
+cb.show_combination_details(combinations_in_budget[0])
+
+cb.execution_time(start)
---
+++ optimized.py
@@ -0,0 +1,138 @@
+"""résout le problème d'optimisation pour maximiser le profit d'un
+investissement dans des actions financières tout en respectant une contrainte
+budgétaire.
+"""
+
+# Import module
+from tqdm import tqdm
+import csv
+import time
+import sys
+
+
+start_time = time.time()
+
+# Vérification de l'investissement personnalisé en numéraire (default = 500)
+try:
+    MAX_INVEST = float(sys.argv[2])
+except IndexError:
+    MAX_INVEST = 500
+
+
+def main():
+    """Vérification de l'entrée du nom de fichier"""
+    try:
+        filename = "data/" + sys.argv[1] + ".csv"
+    except IndexError:
+        print("\nNo filename found. Please try again.\n")
+        time.sleep(1)
+        sys.exit()
+
+    shares_list = read_csv(filename)
+
+    print(f"\nProcessing '{sys.argv[1]}'({len(shares_list)} valid shares) \
+            for {MAX_INVEST} € :")
+
+    display_results(knapsack(shares_list))
+
+
+def read_csv(filename):
+    """Importation de données sur les actions à partir d'un fichier csv
+    Filtrer les données corrompues
+
+    @return: données sur les actions (liste)
+    """
+    try:
+        with open(filename) as csvfile:
+            shares_file = csv.reader(csvfile, delimiter=',')
+
+            if filename != "data/test_shares.csv":
+                next(csvfile)       # skip first row in both datasets
+
+            shares_list = []
+
+            for row in shares_file:
+                if float(row[1]) <= 0 or float(row[2]) <= 0:
+                    pass
+                else:
+                    share = (
+                        row[0],
+                        int(float(row[1])*100),
+                        float(float(row[1]) * float(row[2]) / 100)
+                    )
+                    shares_list.append(share)
+
+            return shares_list
+
+    except FileNotFoundError:
+        print(f"\nFile '{filename}' does not exist. Please try again.\n")
+        time.sleep(1)
+        sys.exit()
+
+
+def knapsack(shares_list):
+    """Initialiser la matrice (ks) pour le problème knapsack 0-1
+    Obtenir la meilleure combinaison d'actions
+
+    @param shares_list: données relatives aux actions (liste)
+    @return: meilleure combinaison possible (liste)
+    """
+    max_inv = int(MAX_INVEST * 100)     # capacity
+    shares_total = len(shares_list)
+    cost = []       # weights
+    profit = []     # values
+
+    for share in shares_list:
+        cost.append(share[1])
+        profit.append(share[2])
+
+    # Trouver le profit optimal
+    ks = [[0 for x in range(max_inv + 1)] for x in range(shares_total + 1)]
+
+    for i in tqdm(range(1, shares_total + 1)):
+
+        for w in range(1, max_inv + 1):
+            if cost[i-1] <= w:
+                ks[i][w] = max(profit[i-1] + ks[i-1][w-cost[i-1]], ks[i-1][w])
+            else:
+                ks[i][w] = ks[i-1][w]
+
+    # Recherche d'une combinaison d'actions à partir d'un profit optimal
+    best_combo = []
+
+    while max_inv >= 0 and shares_total >= 0:
+
+        if ks[shares_total][max_inv] == \
+            ks[shares_total-1][max_inv - cost[shares_total-1]] \
+                + profit[shares_total-1]:
+
+            best_combo.append(shares_list[shares_total-1])
+            max_inv -= cost[shares_total-1]
+
+        shares_total -= 1
+
+    return best_combo
+
+
+def display_results(best_combo):
+    """Afficher les résultats de la meilleure combinaison
+
+    @param best_combo: combinaison d'actions la plus rentable (liste)
+    """
+    print(f"\nInvestissement le plus rentable ({len(best_combo)} Actions) :\n")
+
+    cost = []
+    profit = []
+
+    for item in best_combo:
+        print(f"{item[0]} | {item[1] / 100} € | +{item[2]} €")
+        cost.append(item[1] / 100)
+        profit.append(item[2])
+
+    print("\nTotal cost : ", sum(cost), "€")
+    print("Profit after 2 years : +", sum(profit), "€")
+    print("\nTime elapsed : ", time.time() - start_time, "seconds\n")
+
+
+if __name__ == "__main__":
+    main()
